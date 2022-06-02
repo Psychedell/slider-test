0b19ad39e4fc4fe1e650be2a4611499722c628e4
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-06-02 15:10:19 +0300

    test slisers

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-// $(document).ready(function () {
-//   $('.slider-top').slick({
-//     slidesToShow: 2,
-//     arrows: false,
-//     fade: true,
-//     asNavFor: ".slider",
+$(document).ready(function () {
+  $('.slider-top').slick({
+    slidesToShow: 3,
+    arrows: false,
+    // fade: true,
+    asNavFor: ".slider",
     
-//   });
-// });
+  });
+});
 
-// $(document).ready(function () {
-//   $('.slider').slick({
-//     slidesToShow: 1,
-//     asNavFor:".slider-top"
-//   });
-// });
+$(document).ready(function () {
+  $('.slider').slick({
+    slidesToShow: 1,
+    asNavFor:".slider-top"
+  });
+});
 
 
 // $('.slider').slick({
@@ -25,6 +25,23 @@
 // });
 
 new Swiper(".mySwiper", {
+  slidesPerGroup: 1,
+  slidesPerView: 3,
+  autoHeight: true,
+  // spaceBetween: 32,
+  initialSlide: 1,
+  centeredSlides: true,
+  // touchAngle: 50,
+  slideToClickedSlide: true,
+  loop: true,
+        breakpoints: {
+          320: {
+                  slidesPerView: 1,
+          },  
+          768: {
+                  slidesPerView: 3,
+                }
+          },
         pagination: {
           el: ".swiper-pagination",
           type: "fraction",
